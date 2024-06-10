313a47383f1f8941203e8ec683a23872e0bc9ca4
Monisha Ranjan <ranjan.monisha233@gmail.com>
2024-06-10 11:49:05 -0400

    Updated Resume

--- config/config.js
+++ config/config.js
@@ -36,7 +36,7 @@ export const intro = {
     },
     {
       title: "Resume",
-      link: "https://drive.google.com/file/d/1hqxrt-ljxTAG3Oo-AOJNEOmsQJh0AZsK/view?usp=drive_link",
+      link: "https://drive.google.com/file/d/1H5Cue3FkD0P0su9xKhucd5z6auYedR7E/view?usp=sharing",
       isPrimary: false,
     },
   ],
